ac131e8d65dfa650641b5e59def3f92ab665a2e9
prashant <csprshantphuse@gmail.com>
2022-02-04 19:58:26 +0530

    new branch created

--- src/main/java/com/nt/entity/Student.java
+++ src/main/java/com/nt/entity/Student.java
@@ -2,6 +2,15 @@ package com.nt.entity;
 
 public class Student {
 	private String name;
+	private String sname;
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
 
 	public String getName() {
 		return name;
@@ -9,6 +18,11 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", sname=" + sname + "]";
 	}
 	
 
